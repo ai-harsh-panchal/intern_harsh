18367188af58ad79bbb87835a827eea55754cf8c
ai-harsh-panchal <harsh.panchal@icreativetechnolabs.com>
2025-02-03 14:36:31 +0530

    One2many_Relationship

---
+++ One2many_Assigment/ak_library_management/models/library_member.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class Member(models.Model):
+    _name = 'library.member'
+    _description = 'Library Member Model'
+
+    name = fields.Char(string='Member Name', required=True)
+    email = fields.Char(string='Email ID')
+    phone = fields.Char(string='Contact Number')
+    membership_date = fields.Date(string='Membership Start Date')
+    book_id = fields.Many2one('library.book', string='Book Allocate')
