7b38d611c347a0e1f44b29041768b20785f6db2b
ai-harsh-panchal <harsh.panchal@icreativetechnolabs.com>
2025-02-17 10:11:27 +0530

    Ir.Sequence

---
+++ ir_sequence_Assignment/ak_library_management/models/product_template.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    """
+    This model is inherit for add new field and customize the form view of product template
+    """
+    _inherit = 'product.template'
+
+    is_library_book = fields.Boolean(string='Is Library Book', default=False)
+    author = fields.Char(string='Author Name')
+    publisher = fields.Char(string='Publisher Name')
+    edition = fields.Char(string='Edition')
+    published_date = fields.Date(string='Published Date')
+    pages = fields.Integer(string='Number of Pages')
+    available = fields.Boolean(string='Available in Stock', default=True)
+    barcode = fields.Char(string='ISBN Number', required=True)
+    status = fields.Selection([('available', 'Available'),
+                               ('borrowed', 'Borrowed'),
+                               ('reserved', 'Reserved')],
+                              string='Status')
+
+    def action_borrow(self):
+        """
+        this function is used to set the status borrowed
+        """
+        self.status = 'borrowed'
+        self.available = False
+
+    def action_available(self):
+        """
+        this function is used to set the status back to available from borrowed
+        """
+        self.status = 'available'
+        self.available = True
+
+    def create(self,vals):
+        """
+        this function is used for create a unique sequence in refernce field of product
+        """
+        vals['default_code'] = self.env['ir.sequence'].next_by_code('product.template')
+        return super(ProductTemplate, self).create(vals)
