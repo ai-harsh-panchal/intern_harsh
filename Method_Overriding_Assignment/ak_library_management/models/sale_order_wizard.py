93cbade87cb6a08d586858085853b8f9f4c5880e
ai-harsh-panchal <harsh.panchal@icreativetechnolabs.com>
2025-02-18 19:01:00 +0530

    Method Overriding

--- Method_Overriding_Assignment/ak_library_management/models/sale_order_wizard.py
+++ Method_Overriding_Assignment/ak_library_management/models/sale_order_wizard.py
@@ -4,6 +4,9 @@ from odoo import api, fields, models
 
 
 class SaleOrderWizard(models.TransientModel):
+    """
+    this model is used for only wizard popup message
+    """
     _name = 'sale.order.wizard'
     _description = 'sale order wizard popup'
 
