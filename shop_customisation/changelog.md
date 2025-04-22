1cfa371d282d8e4965e9e156f9be2a85e35fa3c5
ai-harsh-panchal <harsh.panchal@icreativetechnolabs.com>
2025-04-22 12:59:38 +0530

    [MOD] | Assignment Frontend add to cart | manage add to cart button mouse hover in product_template_website xml file |

--- shop_customisation/changelog.md
+++ shop_customisation/changelog.md
@@ -2,8 +2,12 @@
 
 ## [18.0.1.0.0] - 2025-04-18 | Assignment Front-end - Practical Task ('Add to cart' Button)
 
-
 ### Added
 
 - Add website_sale template in product_template_website xml file 
 - add add to cart js file for manage the add to cart functionality
+
+## [18.0.1.0.1] - 2025-04-22 | Assignment frontend practical ( Add to Cart)
+
+### Changes
+- modify in product_template_website xml file manage the add to cart button on mouse hover 
--- shop_customisation/static/src/js/add_to_cart.js
+++ shop_customisation/static/src/js/add_to_cart.js
@@ -1,4 +1,5 @@
 /* @odoo-module */
+
 import publicWidget from '@web/legacy/js/public/public_widget';
 import { rpc } from "@web/core/network/rpc";
 import { cartHandlerMixin } from '@website_sale/js/website_sale_utils';
@@ -8,11 +9,11 @@ publicWidget.registry.ShopPageQuickAddToCart = WebsiteSale.extend(cartHandlerMix
     events: {
         'click .js_add_cart_json': '_onClickQuickAddToCart',
     },
+
     _onClickQuickAddToCart: function (ev) {
         ev.preventDefault();
         const $button = $(ev.currentTarget);
         const productId = $button.data('product-id');
-
         rpc("/shop/product/is_add_to_cart_allowed", {
             product_id: productId,
         }).then((isAllowed) => {
